2753517a3b5925ecba501a89f9c227e2d5a3ee16
ALESSANDRA RAYESFORTE <alerayes@hotmail.com>
2023-08-21 18:36:22 -0700

    Fix styling in some of the pages

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -23,7 +23,7 @@ export default function NavBar() {
       <Disclosure as="nav" className="bg-white shadow">
       {({ open }) => (
         <>
-          <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+          <div className="mx-auto px-4 sm:px-6 lg:px-8">
             <div className="flex h-16 justify-between">
               <div className="flex">
                 <div className="flex flex-shrink-0 items-center">
